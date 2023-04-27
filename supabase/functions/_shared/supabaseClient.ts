bd56ceedb266fc93ae1982a4f921654098f7e897
paddyfink <12916378+paddyfink@users.noreply.github.com>
2023-04-27 07:49:34 -0400

    fix deploy database

--- supabase/functions/_shared/supabaseClient.ts
+++ supabase/functions/_shared/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "../../../app/src/types/database.ts";
 import "dotenv/load";
+import { Database } from "../database.ts";
 
 export const supabaseClient = createClient<Database>(
   // Supabase API URL - env var exported by default when deployed.
