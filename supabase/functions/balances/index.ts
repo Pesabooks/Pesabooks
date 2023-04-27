9ad3c97a75b2c5cce77bd0e3113cee35b98337a0
paddyfink <12916378+paddyfink@users.noreply.github.com>
2023-04-27 07:24:16 -0400

    fix sntr

--- supabase/functions/balances/index.ts
+++ supabase/functions/balances/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { corsHeaders } from "../_shared/cors.ts";
 import { BalanceQuery } from "./type.ts";
-import Sentry from "npm:@sentry/node";
+import * as Sentry from "npm:@sentry/node";
 
 Sentry.init({
   environment: Deno.env.get("ENV") ?? "",
